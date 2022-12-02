2e264c117eccf1bd16bff782281d36d185b95a12
jeremy-deurvillier <88447272+jeremy-deurvillier@users.noreply.github.com>
2022-12-02 19:09:33 +0100

    Ajout de commentaires.

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,20 +1,44 @@
+/* ** Un calendrier minimaliste et configurable.
+ * Par défaut affiche le mois actuel.
+ * Possibilité d'affiché une période donnée via une props "interval".
+ * Boutons "mois suivant" et "mois précédent".
+ * Option pour sélectionner le mois
+ * Option pour sélectionner l'année
+ * Lors du clic sur une date, renvoie la date cliquée via un callback.
+ */
+
 import React, { useState, useEffect } from 'react';
 
+/* ** Fonction composant React.
+ *
+ * @param Array interval Une intervalle représentant la période qu'on veut affichée.
+ */
 export default function Calendar({ interval }) {
-  let today = new Date();
+  let today = new Date(); // Date du jour
 
-  let inf = Math.min(...interval) <= today.getFullYear();
-  let sup = Math.max(...interval) >= today.getFullYear();
+  /* ** Les années dans la propriété interval sont-elles inférieur (inf) et/ou supérieur (sup)
+   * à la date du jour ?
+   */
+  let inf = Math.min(...interval) <= today.getFullYear(); // true si la plus petite année dans interval est <= à l'année actuelle
+  let sup = Math.max(...interval) >= today.getFullYear(); // true si la plus grande année dans interval est >= à l'année actuelle
 
+  /* ** Définition du mois et de l'année à afficher.
+   *   - month : Affiche le mois actuel si l'année actuelle est compris dans interval. Le mois de janvier sinon.
+   *   - year  : Affiche l'année actuelle si cette dernière est compris dans interval. La plus petite année de l'intervalle sinon.
+   */
   const [month, setMonth] = useState(inf && sup ? today.getMonth() : 0);
   const [year, setYear] = useState(
     inf && sup ? today.getFullYear() : Math.min(...interval)
   );
 
+  // Etat des boutons "mois suivant" et "mois précédent".
   const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
   const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
 
-  function prevMonth(e) {
+  /* ** Décrémente le mois actuel de 1.
+   * Passe à l'année précédente si le mois de janvier est affiché.
+   */
+  function prevMonth() {
     if (month - 1 > -1) {
       setMonth((oldMonth) => oldMonth - 1);
     } else {
@@ -23,7 +47,10 @@ export default function Calendar({ interval }) {
     }
   }
 
-  function nextMonth(e) {
+  /* ** Incrémente le mois actuel de 1.
+   * Passe à l'année suivante si le mois de décembre est affiché.
+   */
+  function nextMonth() {
     if (month + 1 < 12) {
       setMonth((oldMonth) => oldMonth + 1);
     } else {
@@ -32,12 +59,20 @@ export default function Calendar({ interval }) {
     }
   }
 
-  // Au montage du composant dans le DOM.
+  /* ** Modification de l'état des boutons "mois suivant" et "mois précédent"
+   * aprés le rendu dans le DOM.
+   *
+   * * Le bouton "mois précédent" est désactivé si est affiché :
+   *   - le mois de janvier ET la plus petite année de l'intervalle.
+   * * Le bouton "mois suivant" est desactivé si est affiché :
+   *   - le mois de décembre ET la plus grande année de l'intervalle.
+   */
   useEffect(() => {
     setPrevBtnDisabled(month === 0 && year === Math.min(...interval));
     setNextBtnDisabled(month === 11 && year === Math.max(...interval));
   }, [month, year, interval]);
 
+  // Rendu
   return (
     <div>
       <p>
@@ -53,6 +88,7 @@ export default function Calendar({ interval }) {
   );
 }
 
+// Propriétés par défaut.
 Calendar.defaultProps = {
-  interval: [2015, 2025],
+  interval: [new Date().getFullYear()],
 };
